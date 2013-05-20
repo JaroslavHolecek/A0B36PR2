a9b1fc0a20e1092e858a63538709c937e2bdae9c
Jarda <Jarda@Jarda-PC>
2013-05-21 00:40:07 +0200

    Dokonceni

--- Maticovy_kalkulator/src/maticovy_kalkulator/Tlacitka.java
+++ Maticovy_kalkulator/src/maticovy_kalkulator/Tlacitka.java
@@ -6,25 +6,26 @@ package maticovy_kalkulator;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Jarda
  */
-public class Tlacitka extends JButton {
-   
-    JLabel displej;
+public class Tlacitka extends JButton {//Tlacitka pouzita v kalkulatoru
+
+    JPanel displej;
+    JLabel op;
     String popis;
     int nacita_uklada; // 0 nic, 1 nacita, 2 uklada
     int seznam_soubor; // 0 nic, i seznam, 2 soubor
-      
-    public Tlacitka(String popis, JLabel displej, int nacita_uklada, int seznam_soubor){
+
+    public Tlacitka(String popis, JPanel displej, JLabel op, int nacita_uklada, int seznam_soubor) {
         super(popis);
         this.popis = popis;
+        this.op = op;
         this.displej = displej;
         this.nacita_uklada = nacita_uklada;
         this.seznam_soubor = seznam_soubor;
     }
 }
-
-    
